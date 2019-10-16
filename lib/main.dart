2c9cb0efac5b503e7b1e5aefaf69a898e725c253
shafinr23 <shafinr23@gmail.com>
2019-10-16 18:22:57 +0600

    add all

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter App',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter App'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            child: Card(
+              child: Text('chart!'),
+              elevation: 5,
+              color: Colors.blueAccent,
+            ),
+          ),
+          Card(
+            child: Text('list of txc'),
+          )
+        ],
+      ),
+    );
+  }
+}
